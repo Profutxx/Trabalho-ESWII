96d3776679833ac7fe03a6729b3b00372621954a
Thiago <thiago@healthmap.com.br>
2021-11-08 22:01:36 -0300

    Adicionado listar Clientes DAO

--- src/DAO/DAOCliente.java
+++ src/DAO/DAOCliente.java
@@ -3,6 +3,16 @@ package DAO;
 import conexoes.ConexaoMySql;
 import model.Cliente;
 
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DAOCliente {
 
     public int salvarCliente(Cliente cliente) {
@@ -29,5 +39,61 @@ public class DAOCliente {
             conexao.fecharConexao();
         }
 
+    }
+
+    public boolean excluirCliente(Cliente cliente) {
+
+        ConexaoMySql conexao = new ConexaoMySql();
+
+        try {
+            conexao.conectar();
+
+            String sql =
+                    "DELETE FROM CLIENTE WHERE cpf = "
+                            + "'" + cliente.getCpf() + "';";
+
+            return conexao.executarUpdateDeleteSQL(sql);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            conexao.fecharConexao();
+        }
+
+    }
+
+    public List<Cliente> listar(){
+
+        List<Cliente> clientes = new ArrayList<>();
+        ConexaoMySql conexao = new ConexaoMySql();
+        PreparedStatement pstm = null;
+        ResultSet rs = null;
+
+        try{
+            String sql = "SELECT * FROM CLIENTE;";
+            pstm = conexao.conectar().prepareStatement(sql);
+            rs = pstm.executeQuery();
+
+            while(rs.next()){
+
+                Cliente cliente = new Cliente();
+
+                cliente.setCpf(rs.getString("cpf"));
+                cliente.setNome(rs.getString("nome"));
+                cliente.setTel(rs.getString("telefone"));
+                cliente.setEndereco(rs.getString("endereco"));
+
+                clientes.add(cliente);
+            }
+
+
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null,"Erro ao listar clientes"+e,"erro",JOptionPane.ERROR_MESSAGE);
+        }finally {
+            conexao.fecharConexao();
+        }
+
+        return clientes;
     }
 }
--- src/controller/ClienteController.java
+++ src/controller/ClienteController.java
@@ -8,5 +8,7 @@ public class ClienteController {
     public int salvarCliente(Cliente cCliente){
         return new DAOCliente().salvarCliente(cCliente);
     }
-
+    public boolean excluirCliente(Cliente cCliente){
+        return new DAOCliente().excluirCliente(cCliente);
+    }
 }
